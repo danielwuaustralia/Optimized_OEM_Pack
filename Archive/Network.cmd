7402b3c939108e0aa30a7960a6a61bae245b6b20
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2021-07-15 23:06:13 +1000

    Delete Network.cmd

--- Archive/Network.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-echo --- Reset Network Profiles
-reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Profiles" /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures" /f
-echo.
-echo --- Disconnect Wireless Lan
-netsh wlan disconnect
-pause
