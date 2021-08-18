0af05738a29dd308b0b44d4c4cb3b15cb961cc44
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2021-08-18 14:38:31 +1000

    Update RemoveAppx.cmd

--- RemoveAppx.cmd
+++ RemoveAppx.cmd
@@ -35,7 +35,6 @@ Microsoft.WindowsStore
 Microsoft.YourPhone
 Microsoft.ZuneMusic
 Microsoft.ZuneVideo
-Microsoft.MicrosoftEdge
 ) do (
 	for /f %%a in ('reg query "%windowsappx%" /f %%i /k 2^>nul ^| find /i "AppxAllUserStore"') do if not errorlevel 1 (reg delete %%a /f 2>nul)
 )
@@ -43,25 +42,24 @@ Microsoft.MicrosoftEdge
 
 set "systemappx=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\InboxApplications"
 for %%i in (
+1527c705-839a-4832-9118-54d4Bd6a0c89
+c5e2524a-ea46-4f67-841f-6a9465d9d515
 E2A4F912-2574-4A75-9BB0-0D023378592B
+F46D4000-FD22-4DB4-AC8E-4E1DDDE828FE
 Microsoft.AAD.BrokerPlugin
 Microsoft.BioEnrollment
 Microsoft.ECApp
 Microsoft.LockApp
-Microsoft.MicrosoftEdge
-Microsoft.MicrosoftEdgeDevToolsClient
-Microsoft.Win32WebViewHost
-F46D4000-FD22-4DB4-AC8E-4E1DDDE828FE
+Microsoft.Windows.Apprep.ChxApp
+Microsoft.Windows.AssignedAccessLockApp
 Microsoft.Windows.CallingShellApp
+Microsoft.Windows.CapturePicker
 Microsoft.Windows.ContentDeliveryManager
-c5e2524a-ea46-4f67-841f-6a9465d9d515
-1527c705-839a-4832-9118-54d4Bd6a0c89
 Microsoft.Windows.ParentalControls
 Microsoft.Windows.PeopleExperienceHost
+Microsoft.Windows.PinningConfirmationDialog
 Microsoft.Windows.SecureAssessmentBrowser
-Microsoft.Windows.SecHealthUI
 Microsoft.Windows.XGpuEjectDialog
-Microsoft.XboxGameCallableUI
 Windows.CBSPreview
 ) do (
 	for /f %%a in ('reg query "%systemappx%" /f %%i /k 2^>nul ^| find /i "AppxAllUserStore"') do if not errorlevel 1 (reg delete %%a /f 2>nul)
