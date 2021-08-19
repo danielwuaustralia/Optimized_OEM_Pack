2404cd04cc0c2f767b593ed998b762e2544d8b83
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2021-08-20 02:13:33 +1000

    Update SetupComplete.cmd

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -8,17 +8,19 @@ auditpol /set /subcategory:"Special Logon" /success:disable
 auditpol /set /subcategory:"Audit Policy Change" /success:disable
 auditpol /set /subcategory:"User Account Management" /success:disable
 
+rem start /wait %windir%\System32\msiexec.exe /package %WINDIR%\Setup\Scripts\SOFTWARE\ /quiet /norestart
+
 %windir%\System32\msiexec.exe /package "%windir%\Setup\Scripts\SOFTWARE\PowerShell.msi" /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
 
 %windir%\System32\msiexec.exe /package "%windir%\Setup\Scripts\SOFTWARE\MicrosoftEdgeEnterpriseX64.msi" /quiet /norestart
+
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\dControl.exe /D
 
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\LAVFilters.exe /VERYSILENT
 
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\java.exe /s
 
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\vulkanruntime.exe /S
-
-start /wait %windir%\System32\msiexec.exe /package %WINDIR%\Setup\Scripts\SOFTWARE\node-v16.6.1-x64.msi /quiet /norestart
 
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VSCode.exe /SP- /VERYSILENT /NORESTART /SUPPRESSMSGBOXES /mergetasks=!runcode
 
@@ -31,6 +33,8 @@ start /wait %WINDIR%\Setup\Scripts\SOFTWARE\processlassoactivator.exe -makekeyfi
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\processlassosetup64.exe /S /keyfile=%WINDIR%\Setup\Scripts\SOFTWARE\prolasso.key /launch_gui=false /gui_start_type=all /governor_start_type=all /language=SimpChinese
 
 start /wait C:\PROGRA~1\UpdateTime\UpdateTime_x64.exe /SI
+
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\Win7GamesForWin10-Setup.exe /S /nouninstaller /lang=SimpChinese
 
 "%windir%\Setup\Scripts\Office365\setup.exe" /configure "%windir%\Setup\Scripts\Office365\Configuration.xml"
 
