815f4a806e9a6c3380601a4454937fc06619a8dd
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2021-08-29 13:53:49 +1000

    Update SetupComplete.cmd

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -1,16 +1,49 @@
 @echo off
 powercfg -h off
-rem net accounts /maxpwage:unlimited
+rem account policy
 FOR /F "TOKENS=2 DELIMS==" %%A IN ('"WMIC /NAMESPACE:\\ROOT\CIMV2 PATH Win32_UserAccount GET Name /VALUE"') DO (
 CALL WMIC /NAMESPACE:\\ROOT\CIMV2 PATH Win32_UserAccount WHERE Name='%%A' SET PasswordExpires=FALSE
 )
+rem safety policy
 auditpol /set /subcategory:"Special Logon" /success:disable
 auditpol /set /subcategory:"Audit Policy Change" /success:disable
 auditpol /set /subcategory:"User Account Management" /success:disable
 
-rem start /wait %windir%\System32\msiexec.exe /package %WINDIR%\Setup\Scripts\SOFTWARE\ /quiet /norestart
+rem VC Redist
+start /wait %WINDIR%\Setup\Scripts\Redist\2005\vcredist_x86.exe /Q
 
-%windir%\System32\msiexec.exe /package "%windir%\Setup\Scripts\SOFTWARE\PowerShell.msi" /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
+start /wait %WINDIR%\Setup\Scripts\Redist\2005\vcredist_x64.exe /Q
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2008\vcredist_x86.exe /Q
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2008\vcredist_x64.exe /Q
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2010\vcredist_x86.exe /q /norestart
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2010\vcredist_x64.exe /q /norestart
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2012\vcredist_x86.exe /install /quiet /norestart
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2012\vcredist_x64.exe /install /quiet /norestart
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2013\vcredist_x86.exe /install /quiet /norestart
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2013\vcredist_x64.exe /install /quiet /norestart
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2015\vc_redist.x86.exe /q /norestart
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2015\vc_redist.x64.exe /q /norestart
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2017\vc_redist.x86.exe /q /norestart
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2017\vc_redist.x64.exe /q /norestart
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2019\VC_redist.x86.exe /q /norestart
+
+start /wait %WINDIR%\Setup\Scripts\Redist\2019\VC_redist.x64.exe /q /norestart
+
+rem install apps
+%windir%\System32\msiexec.exe /package "%windir%\Setup\Scripts\SOFTWARE\PowerShell-7.2.0-preview.9-win-x64.msi" /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
 
 %windir%\System32\msiexec.exe /package "%windir%\Setup\Scripts\SOFTWARE\MicrosoftEdgeEnterpriseX64.msi" /quiet /norestart
 
@@ -24,7 +57,7 @@ start /wait %WINDIR%\Setup\Scripts\SOFTWARE\vulkanruntime.exe /S
 
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VSCode.exe /SP- /VERYSILENT /NORESTART /SUPPRESSMSGBOXES /mergetasks=!runcode
 
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VisualCppRedist_AIO_x86_x64.exe /ai58X23TEV
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\AMD_Chipset_Software.exe /S
 
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\7z2103-x64.exe /S
 
@@ -37,8 +70,6 @@ start /wait %WINDIR%\Setup\Scripts\SOFTWARE\processlassoactivator.exe -makekeyfi
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\processlassosetup64.exe /S /keyfile=%WINDIR%\Setup\Scripts\SOFTWARE\prolasso.key /launch_gui=false /gui_start_type=all /governor_start_type=all /language=SimpChinese
 
 start /wait C:\PROGRA~1\UpdateTime\UpdateTime_x64.exe /SI
-
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\Win7GamesForWin10-Setup.exe /S /nouninstaller /lang=SimpChinese
 
 "%windir%\Setup\Scripts\Office365\setup.exe" /configure "%windir%\Setup\Scripts\Office365\Configuration.xml"
 
