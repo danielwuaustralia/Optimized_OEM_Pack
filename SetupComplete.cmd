307b6dd7f69b6f7f114d677c61f3f0c5516b7bd9
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2021-09-18 00:48:44 +1000

    Update SetupComplete.cmd

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -9,73 +9,64 @@ auditpol /set /subcategory:"Special Logon" /success:disable
 auditpol /set /subcategory:"Audit Policy Change" /success:disable
 auditpol /set /subcategory:"User Account Management" /success:disable
 
-rem VC Redist
-start /wait %WINDIR%\Setup\Scripts\Redist\2005\vcredist_x86.exe /Q
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2005\vcredist_x64.exe /Q
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2008\vcredist_x86.exe /Q
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2008\vcredist_x64.exe /Q
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2010\vcredist_x86.exe /q /norestart
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2010\vcredist_x64.exe /q /norestart
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2012\vcredist_x86.exe /install /quiet /norestart
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2012\vcredist_x64.exe /install /quiet /norestart
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2013\vcredist_x86.exe /install /quiet /norestart
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2013\vcredist_x64.exe /install /quiet /norestart
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2015\vc_redist.x86.exe /q /norestart
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2015\vc_redist.x64.exe /q /norestart
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2017\vc_redist.x86.exe /q /norestart
-
-start /wait %WINDIR%\Setup\Scripts\Redist\2017\vc_redist.x64.exe /q /norestart
-
-rem Direct X 10
-start /wait %WINDIR%\Setup\Scripts\DirectX\DXSETUP.exe /silent
-
 rem Powershell 7
 %windir%\System32\msiexec.exe /package "%windir%\Setup\Scripts\SOFTWARE\PowerShell-7.2.0-preview.9-win-x64.msi" /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
 
+rem VC Redist
+start /wait %WINDIR%\Setup\Scripts\Runtime\2005x64.exe /Q
+start /wait %WINDIR%\Setup\Scripts\Runtime\2008x64.exe /Q
+start /wait %WINDIR%\Setup\Scripts\Runtime\2010x64.exe /q /norestart
+start /wait %WINDIR%\Setup\Scripts\Runtime\2012x64.exe /install /quiet /norestart
+start /wait %WINDIR%\Setup\Scripts\Runtime\2013x64.exe /install /quiet /norestart
+
+rem Direct X 10
+start /wait %WINDIR%\Setup\Scripts\Runtime\DirectX\DXSETUP.exe /silent
+
 rem Edge Chrome
-
 %windir%\System32\msiexec.exe /package "%windir%\Setup\Scripts\SOFTWARE\MicrosoftEdgeEnterpriseX64.msi" /quiet /norestart
-
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\dControl.exe /D
-
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\LAVFilters-0.75.1-3.exe /VERYSILENT
-
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\java.exe /s
-
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VulkanRT-1.2.189.1-Installer.exe /S
-
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VSCodeSetup-x64-1.61.0-insider.exe /SP- /VERYSILENT /NORESTART /SUPPRESSMSGBOXES /mergetasks=!runcode
-
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\XnView-win-full.exe /VERYSILENT /SUPPRESSMSGBOXES
-
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\AMD_Chipset_Software.exe /S
-
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\7z2103-x64.exe /S
-
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\MicrosoftEdgeWebView2RuntimeInstallerX64.exe /silent /install
 
-rem start /wait %WINDIR%\Setup\Scripts\SOFTWARE\TranslucentTB.exe /VERYSILENT /SP- /SUPPRESSMSGBOXES
+rem disable defender
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\dControl.exe /D
 
+rem lav
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\LAVFilters-0.75.1-4.exe /VERYSILENT
+
+rem java runtime
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\java.exe /s
+
+rem vulkan runtime
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VulkanRT-1.2.189.2-Installer.exe /S
+
+rem VSCode
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VSCodeSetup-x64-1.61.0-insider.exe /SP- /VERYSILENT /NORESTART /SUPPRESSMSGBOXES /mergetasks=!runcode
+
+rem xnview
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\XnViewMP-win-x64.exe /VERYSILENT /SUPPRESSMSGBOXES
+
+rem chipset driver
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\AMD_Chipset_Software.exe /S
+
+rem 7zip
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\7z2103-x64.exe /S
+
+rem process lasso
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\processlassoactivator.exe -makekeyfile -product:2 -output:"%WINDIR%\Setup\Scripts\SOFTWARE"
-
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\processlassosetup64.exe /S /keyfile=%WINDIR%\Setup\Scripts\SOFTWARE\prolasso.key /launch_gui=false /gui_start_type=all /governor_start_type=all /language=SimpChinese
 
+rem time sync
 start /wait C:\PROGRA~1\UpdateTime\UpdateTime_x64.exe /SI
 
+rem TranslucentTB
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\TranslucentTB.exe /VERYSILENT /SP- /SUPPRESSMSGBOXES
+
+rem Nvidia Driver
+start /wait %WINDIR%\Setup\Scripts\Driver\setup.exe /s
+
+rem Office LTSC
 "%windir%\Setup\Scripts\Office365\setup.exe" /configure "%windir%\Setup\Scripts\Office365\Configuration.xml"
 
+rem KMS
 call %windir%\Setup\Scripts\KMS.cmd
 
 %windir%\System32\UsoClient.exe RefreshSettings
