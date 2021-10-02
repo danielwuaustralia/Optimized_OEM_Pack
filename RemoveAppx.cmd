b2a86f82bd556f7d0ad9ee47053cf268d796ccfe
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2021-10-02 17:26:50 +1000

    Update RemoveAppx.cmd

--- RemoveAppx.cmd
+++ RemoveAppx.cmd
@@ -6,22 +6,18 @@ Microsoft.549981C3F5F10
 Microsoft.BingNews
 Microsoft.BingWeather
 Microsoft.DesktopAppInstaller
-Microsoft.GamingApp
 Microsoft.GetHelp
 Microsoft.Getstarted
-Microsoft.HEIFImageExtension
 Microsoft.MicrosoftOfficeHub
 Microsoft.MicrosoftSolitaireCollection
 Microsoft.MicrosoftStickyNotes
+Microsoft.PowerAutomateDesktop
 Microsoft.Paint
 Microsoft.People
 Microsoft.ScreenSketch
 Microsoft.SecHealthUI
 Microsoft.StorePurchaseApp
 Microsoft.Todos
-Microsoft.VP9VideoExtensions
-Microsoft.WebMediaExtensions
-Microsoft.WebpImageExtension
 Microsoft.Windows.Photos
 Microsoft.WindowsAlarms
 Microsoft.WindowsCalculator
@@ -30,14 +26,13 @@ microsoft.windowscommunicationsapps
 Microsoft.WindowsFeedbackHub
 Microsoft.WindowsMaps
 Microsoft.WindowsNotepad
-Microsoft.WindowsSoundRecorder
+Microsoft.WindowsStore
 Microsoft.WindowsTerminal
-Microsoft.Xbox.TCUI
-Microsoft.XboxGamingOverlay
-Microsoft.XboxIdentityProvider
+Microsoft.WindowsSoundRecorder
 Microsoft.YourPhone
 Microsoft.ZuneMusic
 Microsoft.ZuneVideo
+MicrosoftWindows.Client.WebExperience
 ) do (
 	for /f %%a in ('reg query "%windowsappx%" /f %%i /k 2^>nul ^| find /i "AppxAllUserStore"') do if not errorlevel 1 (reg delete %%a /f 2>nul)
 )
@@ -45,17 +40,23 @@ Microsoft.ZuneVideo
 
 set "systemappx=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\InboxApplications"
 for %%i in (
+E2A4F912-2574-4A75-9BB0-0D023378592B
 1527c705-839a-4832-9118-54d4Bd6a0c89
 c5e2524a-ea46-4f67-841f-6a9465d9d515
 F46D4000-FD22-4DB4-AC8E-4E1DDDE828FE
 Microsoft.AAD.BrokerPlugin
-Microsoft.AsyncTextService
 Microsoft.BioEnrollment
+Microsoft.Windows.CallingShellApp
+Microsoft.Windows.CapturePicker
+Microsoft.Windows.ContentDeliveryManager
 Microsoft.ECApp
 Microsoft.LockApp
+Microsoft.Windows.PinningConfirmationDialog
+Microsoft.Windows.AppRep.ChxApp
 Microsoft.Windows.ParentalControls
 Microsoft.Windows.PeopleExperienceHost
-NcsiUwpApp
+Microsoft.Windows.SecureAssessmentBrowser
+Microsoft.Windows.XGpuEjectDialog
 Windows.CBSPreview
 ) do (
 	for /f %%a in ('reg query "%systemappx%" /f %%i /k 2^>nul ^| find /i "AppxAllUserStore"') do if not errorlevel 1 (reg delete %%a /f 2>nul)
