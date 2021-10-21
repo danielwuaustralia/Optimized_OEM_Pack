87ac372a7ffcdf2ac6884f3d261eb4709e4bf1ff
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2021-10-21 21:18:34 +1100

    Update SetupComplete.cmd

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -22,14 +22,14 @@ start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VulkanRT-1.2.189.2-Installer.exe /S
 rem Visual Runtime
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VisualCppRedist_AIO_x86_x64.exe /ai /gm2
 
+rem DirectX Runtime
+start /wait %WINDIR%\Setup\Scripts\DirectX\DXSETUP.exe /silent
+
 rem VSCode
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VSCodeSetup-x64-1.62.0-insider.exe /SP- /VERYSILENT /NORESTART /SUPPRESSMSGBOXES /mergetasks=!runcode
 
 rem xnview
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\XnViewMP-win-x64.exe /VERYSILENT /SUPPRESSMSGBOXES
-
-rem Startisback
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\StartAllBack_v2.9.94_Repack_for_Win11.exe /ai /gm2
 
 rem chipset driver
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\AMD_Chipset_Software.exe /S
@@ -41,9 +41,6 @@ start /wait %WINDIR%\Setup\Scripts\SOFTWARE\processlassosetup64.exe /S /keyfile=
 rem time sync
 start /wait C:\PROGRA~1\UpdateTime\UpdateTime_x64.exe /SI
 
-rem windows 7 games
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\Windows7Games.exe /S
-
 rem Nvidia Driver
 start /wait %WINDIR%\Setup\Scripts\Driver\setup.exe /s
 
@@ -53,11 +50,7 @@ rem Office LTSC
 rem KMS
 call %windir%\Setup\Scripts\KMS.cmd
 
-rem disable all power saving feature
-"C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -NonInteractive -ExecutionPolicy Bypass "%WINDIR%\Setup\Scripts\DisablePowerSaving.ps1"
-
 rem remove Edge
 "C:\Program Files (x86)\Microsoft\Edge\Application\92.0.902.67\Installer\setup.exe" --uninstall --system-level --verbose-logging --force-uninstall --delete-profile
-rem call %WINDIR%\Setup\Scripts\RemoveEdge.cmd
 
 %windir%\System32\UsoClient.exe RefreshSettings
