0259eed481a2b46e5bc1ac8dc3cd542352df5d1b
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2021-10-28 19:56:45 +1100

    Update RemoveAppx.cmd

--- RemoveAppx.cmd
+++ RemoveAppx.cmd
@@ -5,9 +5,9 @@ for %%i in (
 Microsoft.549981C3F5F10
 Microsoft.BingNews
 Microsoft.BingWeather
-Microsoft.DesktopAppInstaller
 Microsoft.GetHelp
 Microsoft.Getstarted
+Microsoft.GamingApp
 Microsoft.MicrosoftOfficeHub
 Microsoft.MicrosoftSolitaireCollection
 Microsoft.MicrosoftStickyNotes
@@ -37,14 +37,13 @@ Microsoft.MicrosoftEdge.Stable
 	for /f %%a in ('reg query "%windowsappx%" /f %%i /k 2^>nul ^| find /i "AppxAllUserStore"') do if not errorlevel 1 (reg delete %%a /f 2>nul)
 )
 
-
+rem AADBrokerPlugin - needed for Microsoft Account, Windows Store Client - needed for Microsoft DCH drivers
 set "systemappx=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\InboxApplications"
 for %%i in (
 E2A4F912-2574-4A75-9BB0-0D023378592B
 1527c705-839a-4832-9118-54d4Bd6a0c89
 c5e2524a-ea46-4f67-841f-6a9465d9d515
 F46D4000-FD22-4DB4-AC8E-4E1DDDE828FE
-Microsoft.AAD.BrokerPlugin
 Microsoft.AsyncTextService
 Microsoft.BioEnrollment
 Microsoft.Windows.AssignedAccessLockApp
