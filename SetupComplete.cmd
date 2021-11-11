9c43abae8c75e52b787b9d44bffcab95688d6ef0
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2021-11-11 19:54:34 +1100

    Update SetupComplete.cmd

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -1,9 +1,11 @@
 @setlocal DisableDelayedExpansion
 @echo off
+
 rem Office LTSC
 "%windir%\Setup\Scripts\Office365\setup.exe" /configure "%windir%\Setup\Scripts\Office365\Configuration.xml"
 
-powercfg -h off
+rem KMS
+call %windir%\Setup\Scripts\KMS.cmd
 
 rem account policy
 FOR /F "TOKENS=2 DELIMS==" %%A IN ('"WMIC /NAMESPACE:\\ROOT\CIMV2 PATH Win32_UserAccount GET Name /VALUE"') DO (
@@ -14,11 +16,16 @@ auditpol /set /subcategory:"Special Logon" /success:disable
 auditpol /set /subcategory:"Audit Policy Change" /success:disable
 auditpol /set /subcategory:"User Account Management" /success:disable
 
+rem register video filters
+regsvr32.exe "%windir%\SysWOW64\XySubFilter.dll" /s
+regsvr32.exe "%windir%\SysWOW64\VSFilter.dll" /s
+regsvr32.exe "C:\Program Files\madVR\madVR64.ax" /s
+
 rem Powershell 7
-%windir%\System32\msiexec.exe /package "%windir%\Setup\Scripts\SOFTWARE\PowerShell-7.2.0-rc.1-win-x64.msi" /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
+%windir%\System32\msiexec.exe /package "%windir%\Setup\Scripts\SOFTWARE\PowerShell-7.2.0-win-x64.msi" /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
 
 rem lav
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\LAVFilters-0.75.1-9.exe /VERYSILENT
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\LAVFilters-0.75.1-10.exe /VERYSILENT
 
 rem vulkan runtime
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VulkanRT-1.2.189.2-Installer.exe /S
@@ -30,13 +37,13 @@ rem DirectX Runtime
 start /wait %WINDIR%\Setup\Scripts\DirectX\DXSETUP.exe /silent
 
 rem Net Framework
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\windowsdesktop-runtime-5.0.11-win-x64.exe /quiet /norestart
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\windowsdesktop-runtime-6.0.0-win-x64.exe /quiet /norestart
 
 rem VSCode
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VSCodeSetup-x64-1.62.0-insider.exe /SP- /VERYSILENT /NORESTART /SUPPRESSMSGBOXES /mergetasks=!runcode
 
 rem chipset driver
-start /wait %WINDIR%\Setup\Scripts\SOFTWARE\amd_chipset_software_3.10.08.506.exe /S
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\AMD_Chipset_Drivers_3.10.22.706.exe /S
 
 rem process lasso
 start /wait %WINDIR%\Setup\Scripts\SOFTWARE\processlassoActivator.exe -makekeyfile -product:2 -output:"%WINDIR%\Setup\Scripts\SOFTWARE"
