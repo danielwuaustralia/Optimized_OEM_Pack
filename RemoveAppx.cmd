346f5e1d5ea3dbc16b5a04a3e830dbdbdf2ec082
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2022-01-07 21:32:28 +1100

    Update RemoveAppx.cmd

--- RemoveAppx.cmd
+++ RemoveAppx.cmd
@@ -42,28 +42,29 @@ Microsoft.MicrosoftEdge.Stable
 rem AADBrokerPlugin - needed for Microsoft Account, Windows Store Client - needed for Microsoft DCH drivers
 set "systemappx=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\InboxApplications"
 for %%i in (
-E2A4F912-2574-4A75-9BB0-0D023378592B
 1527c705-839a-4832-9118-54d4Bd6a0c89
 c5e2524a-ea46-4f67-841f-6a9465d9d515
 F46D4000-FD22-4DB4-AC8E-4E1DDDE828FE
+Microsoft.AAD.BrokerPlugin
 Microsoft.AsyncTextService
 Microsoft.BioEnrollment
+Microsoft.ECApp
+Microsoft.LockApp
+Microsoft.MicrosoftEdge
+Microsoft.MicrosoftEdgeDevToolsClient
+Microsoft.Win32WebViewHost
+Microsoft.Windows.Apprep.ChxApp
 Microsoft.Windows.AssignedAccessLockApp
 Microsoft.Windows.CallingShellApp
 Microsoft.Windows.CapturePicker
 Microsoft.Windows.ContentDeliveryManager
-Microsoft.ECApp
-Microsoft.LockApp
-Microsoft.Windows.PinningConfirmationDialog
-Microsoft.Windows.AppRep.ChxApp
+Microsoft.Windows.NarratorQuickStart
 Microsoft.Windows.ParentalControls
 Microsoft.Windows.PeopleExperienceHost
+Microsoft.Windows.PinningConfirmationDialog
 Microsoft.Windows.SecureAssessmentBrowser
 Microsoft.Windows.XGpuEjectDialog
-Microsoft.MicrosoftEdge
-Microsoft.MicrosoftEdgeDevToolsClient
-Microsoft.Win32WebViewHost
-Windows.CBSPreview
+Microsoft.XboxGameCallableUI
 ) do (
 	for /f %%a in ('reg query "%systemappx%" /f %%i /k 2^>nul ^| find /i "AppxAllUserStore"') do if not errorlevel 1 (reg delete %%a /f 2>nul)
 )
