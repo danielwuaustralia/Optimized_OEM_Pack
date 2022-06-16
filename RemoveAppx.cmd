752de69946eb04990f8baa8e13c80409372240ce
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2022-06-16 15:21:35 +1000

    Update RemoveAppx.cmd

--- RemoveAppx.cmd
+++ RemoveAppx.cmd
@@ -1,29 +1,49 @@
+@cls
 @echo off
+>nul chcp 437
+setlocal enabledelayedexpansion
+rem Setupcomplete.cmd is ran after oobe and just before the desktop appears when using windows setup.
+rem Oobe.cmd is run after the the screen about copying of files/expanding files part is shown via setup and the bit when it says it will reboot in x seconds...
 
-set "windowsappx=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Applications"
-for %%i in (
-Microsoft.SecHealthUI
-Microsoft.MicrosoftEdge.Stable
-) do (
-	for /f %%a in ('reg query "%windowsappx%" /f %%i /k 2^>nul ^| find /i "AppxAllUserStore"') do if not errorlevel 1 (reg delete %%a /f 2>nul)
-)
+rem Office 365
+"%windir%\Setup\Scripts\Office365\setup.exe" /configure "%windir%\Setup\Scripts\Office365\O365Preview.xml"
 
-rem AADBrokerPlugin - modern authentication
-rem ContentDeliveryManager - this component is responsible for the automatic installation of unwanted advertised applications
-rem Microsoft Accounts Control - Microsoft Accounts Control app (This could prevent sign to microsoft account in UWP apps)
-rem CloudExperienceHost - https://docs.microsoft.com/en-us/office365/troubleshoot/authentication/automatic-authentication-fails
-rem Xbox Game Callable UI - Xbox Game Callable UI App (Breaks Xbox Live Games)
-rem MicrosoftWindows.Client.CBS - This prevent Snip & Sketch work and maybe Mail and Calendar
-rem Microsoft.549981C3F5F10 required for store
-rem Windows.CBSPreview - https://docs.microsoft.com/en-us/windows/uwp/devices-sensors/pos-camerabarcode
-set "systemappx=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\InboxApplications"
-for %%i in (
-Microsoft.BioEnrollment
-Microsoft.MicrosoftEdge
-Microsoft.MicrosoftEdgeDevToolsClient
-Microsoft.Win32WebViewHost
-Microsoft.Windows.ParentalControls
-Microsoft.Windows.PeopleExperienceHost
-) do (
-	for /f %%a in ('reg query "%systemappx%" /f %%i /k 2^>nul ^| find /i "AppxAllUserStore"') do if not errorlevel 1 (reg delete %%a /f 2>nul)
-)
+rem KMS
+call %windir%\Setup\Scripts\KMS_VL_ALL_AIO.cmd /s /a
+
+rem register video filters
+regsvr32.exe "C:\Program Files\madVR\madVR64.ax" /s
+
+rem 7zip
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\7z2107-x64.exe /S /D="C:\Program Files\7-Zip"
+
+rem https://docs.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VisualCppRedist_AIO_x86_x64.exe /ai /gm2
+
+rem Powershell 7
+%windir%\System32\msiexec.exe /package "%windir%\Setup\Scripts\SOFTWARE\PowerShell-7.3.0-preview.4-win-x64.msi" /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
+
+rem lav
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\LAVFilters-0.76.1-3.exe /VERYSILENT
+
+rem vulkan runtime
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\VulkanRT-1.3.216.0-Installer.exe /S
+
+rem Direct X
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\DirectXRedist.exe /ai
+
+rem process lasso
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\processlassoActivator.exe -makekeyfile -product:2 -output:"%WINDIR%\Setup\Scripts\SOFTWARE"
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\processlassosetup64.exe /S /keyfile=%WINDIR%\Setup\Scripts\SOFTWARE\prolasso.key /launch_gui=false /gui_start_type=all /governor_start_type=all /language=SimpChinese
+
+rem chipset driver
+start /wait %WINDIR%\Setup\Scripts\SOFTWARE\AMD_Chipset_Software.exe /S
+
+rem time sync
+start /wait C:\PROGRA~1\UpdateTime\UpdateTime_x64.exe /SI
+
+rem remove Edge
+"C:\Program Files (x86)\Microsoft\EdgeUpdate\MicrosoftEdgeUpdate.exe" /uninstall
+
+del /f /q C:\Windows\Panther\unattend.xml
+%windir%\System32\UsoClient.exe RefreshSettings
