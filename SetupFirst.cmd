a6be16cfd7d51908902bd53ddbd41bb8b9c98bbe
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2022-07-29 18:12:04 +1000

    Update SetupFirst.cmd

--- SetupFirst.cmd
+++ SetupFirst.cmd
@@ -64,6 +64,8 @@ Dism /online /Disable-Feature /FeatureName:ServerCore-Drivers-General /NoRestart
 
 Dism /online /Disable-Feature /FeatureName:ServerCore-Drivers-General-WOW64 /NoRestart
 
+Dism /online /Disable-Feature /FeatureName:SearchEngine-Client-Package /NoRestart
+
 Dism /Online /Remove-Capability /CapabilityName:App.StepsRecorder~~~~0.0.1.0 /NoRestart
 
 Dism /Online /Remove-Capability /CapabilityName:DirectX.Configuration.Database~~~~0.0.1.0 /NoRestart
@@ -92,10 +94,30 @@ REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\UnattendSettings\PnPU
 
 rem windows update
 set "key=HKLM\SYSTEM\ControlSet001\Control\Ubpm"
-for /f "tokens=1" %%a in ('reg query "%key%" 2^>nul ^| find /i "REG_SZ"') do reg delete %key% /v "%%a" /f 2
+for /f "tokens=1" %%a in ('reg query "%key%" 2^>nul ^| find /i "REG_SZ"') do reg delete %key% /v "%%a" /f 2>nul
 
 set "key=HKLM\Software\Microsoft\WindowsUpdate\Orchestrator\UScheduler_Oobe"
-for /f %%a in ('reg query "%key%" /f * /k 2^>nul ^| find /i "Orchestrator"') do reg delete %%a /f 2
+for /f %%a in ('reg query "%key%" /f * /k 2^>nul ^| find /i "Orchestrator"') do reg delete %%a /f 2>nul
+
+C:\Windows\SysWOW64\SetACL.exe -on "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing" -ot reg -actn ace -ace "n:S-1-5-32-544;p:full;i:np"
+C:\Windows\SysWOW64\SetACL.exe -on "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing" -ot reg -actn ace -ace "n:S-1-5-32-544;p:full;i:sc,io;m:grant"
+C:\Windows\SysWOW64\SetACL.exe -on "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Update" -ot reg -actn ace -ace "n:S-1-5-32-544;p:full;i:np"
+C:\Windows\SysWOW64\SetACL.exe -on "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Update" -ot reg -actn ace -ace "n:S-1-5-32-544;p:full;i:sc,io;m:grant"
+reg query "HKLM\SOFTWARE\Microsoft\Windows\Windows NT\CurrentVersion\EditionVersion" && (
+C:\Windows\SysWOW64\SetACL.exe -on "HKLM\SOFTWARE\Microsoft\Windows\Windows NT\CurrentVersion\EditionVersion" -ot reg -actn ace -ace "n:S-1-5-32-544;p:full;i:np"
+C:\Windows\SysWOW64\SetACL.exe -on "HKLM\SOFTWARE\Microsoft\Windows\Windows NT\CurrentVersion\EditionVersion" -ot reg -actn ace -ace "n:S-1-5-32-544;p:full;i:sc,io;m:grant"
+)
+C:\Windows\SysWOW64\SetACL.exe -on "HKLM\SOFTWARE\Classes\xmlfile\CLSID" -ot reg -actn ace -ace "n:S-1-5-32-544;p:full;i:np"
+C:\Windows\SysWOW64\SetACL.exe -on "HKLM\SOFTWARE\Classes\xmlfile\CLSID" -ot reg -actn ace -ace "n:S-1-5-32-544;p:full;i:sc,io;m:grant"
+C:\Windows\SysWOW64\SetACL.exe -on "%windir%\Logs\CBS" -ot file -actn ace -ace "n:S-1-5-32-544;p:full"
+md "%windir%\Logs\WaaSMedic"
+C:\Windows\SysWOW64\SetACL.exe -on "%windir%\Logs\WaaSMedic" -ot file -actn ace -ace "n:S-1-5-32-544;p:full"
+C:\Windows\SysWOW64\SetACL.exe -on "%windir%\WinSxS\Backup" -ot file -actn ace -ace "n:S-1-5-32-544;p:full"
+C:\Windows\SysWOW64\SetACL.exe -on "%ProgramData%\Microsoft\Diagnosis" -ot file -actn ace -ace "n:S-1-5-32-544;p:full;i:so,sc,io"
+del /f /q /s "%ProgramData%\Microsoft\Diagnosis\ETLLogs\*"
+del /f /q "%ProgramData%\Microsoft\Diagnosis\*.rbs"
+sc triggerinfo wuauserv delete
+sc triggerinfo WaaSMedicSvc delete
 
 rem Needed for: Windows Store
 rem DesktopAppInstaller - is interesting to keep for working winget commands.
@@ -110,3 +132,33 @@ rem XboxIdentityProvider Xbox Identity Provider - Require for sign in to Xbox Co
 rem XboxClient - required to xbox app work
 rem XboxApp "w10" equal GamingApp "w11" - Xbox Console Companion
 rem XboxSpeechToTextOverlay
+
+set "Applications=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Applications"
+for %%i in (
+Microsoft.SecHealthUI
+Microsoft.MicrosoftEdge.Stable
+) do (
+	for /f %%a in ('reg query "%Applications%" /f %%i /k 2^>nul ^| find /i "AppxAllUserStore"') do if not errorlevel 1 (reg delete %%a /f 2>nul)
+)
+
+set "InboxApplications=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\InboxApplications"
+for %%i in (
+MicrosoftEdge
+MicrosoftEdgeDevToolsClient
+AppRep.ChxApp
+BioEnrollment
+ContentDeliveryManager
+Microsoft.ECApp
+narratorquickstart
+OOBENetworkCaptivePortal
+OOBENetworkConnectionFlow
+ParentalControls
+PeopleExperienceHost
+PrintDialog
+SecondaryTileExperience
+SecureAssessmentBrowser
+XboxGameCallableUI
+Microsoft.Win32WebViewHost
+) do (
+for /f %%a in ('reg query "%InboxApplications%" /f %%i /k 2^>nul ^| find /i "AppxAllUserStore"') do if not errorlevel 1 (reg delete %%a /f 2>nul)
+)
