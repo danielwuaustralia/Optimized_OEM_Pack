238e524851386ae8d2096bcd3891a3d0376da268
Daniel Wu <16550029+DiChaoWu@users.noreply.github.com>
2022-09-09 13:52:32 +1000

    Update SetupFirst.cmd

--- SetupFirst.cmd
+++ SetupFirst.cmd
@@ -97,6 +97,26 @@ Microsoft.Win32WebViewHost
 AppRep.ChxApp
 Microsoft.BioEnrollment
 Microsoft.Windows.PeopleExperienceHost
+Microsoft.AAD.BrokerPlugin
+Microsoft.AsyncTextService
+Microsoft.ECApp
+Microsoft.LockApp
+Microsoft.Windows.AssignedAccessLockApp
+Microsoft.Windows.CapturePicker
+Microsoft.Windows.ContentDeliveryManager
+Microsoft.Windows.NarratorQuickStart
+Microsoft.Windows.OOBENetworkCaptivePortal
+Microsoft.Windows.OOBENetworkConnectionFlow
+Microsoft.Windows.ParentalControls
+Microsoft.Windows.PinningConfirmationDialog
+Microsoft.Windows.PrintQueueActionCenter
+Microsoft.Windows.XGpuEjectDialog
+Microsoft.XboxGameCallableUI
+Windows.CBSPreview
+1527c705-839a-4832-9118-54d4Bd6a0c89
+c5e2524a-ea46-4f67-841f-6a9465d9d515
+E2A4F912-2574-4A75-9BB0-0D023378592B
+F46D4000-FD22-4DB4-AC8E-4E1DDDE828FE
 ) do (
 for /f %%a in ('reg query "%InboxApplications%" /f %%i /k 2^>nul ^| find /i "AppxAllUserStore"') do if not errorlevel 1 (reg delete %%a /f 2>nul)
 )
