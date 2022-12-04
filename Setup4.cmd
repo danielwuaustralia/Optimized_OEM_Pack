80549163e1d1ca6945a0f4b79fc7965efa26f8a3
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2022-12-05 03:12:01 +1100

    Delete Setup4.cmd

--- Setup4.cmd
+++
@@ -1,7 +0,0 @@
-@cls
-@echo on
->nul chcp 437
-setlocal enabledelayedexpansion
-
-move "C:\Windows\Runstartuptask.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup" /y
-del /f /q C:\Windows\Setup\Scripts\SetupComplete.cmd
