c597d07508d3447014299d61f0504e1a6c5917c6
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2023-03-09 20:27:27 +1100

    Update SetupComplete.cmd

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -1,7 +1,5 @@
-@cls
 @echo off
->nul chcp 437
-setlocal enabledelayedexpansion
+setlocal EnableDelayedExpansion
 
 rem Office 365
 "C:\TEMP\Office365\setup.exe" /configure "C:\TEMP\Office365\O365Preview.xml"
@@ -51,56 +49,6 @@ rem https://learn.microsoft.com/en-us/troubleshoot/windows-server/performance/ma
 lodctr /r
 lodctr /r
 winmgmt /resyncperf
-
-rem taskscheduler
-for %%a in (
-    "\Microsoft\Windows\ApplicationData\appuriverifierdaily"
-    "\Microsoft\Windows\ApplicationData\appuriverifierinstall"
-    "\Microsoft\Windows\ApplicationData\DsSvcCleanup"
-    "\Microsoft\Windows\Application Experience\PcaPatchDbTask"
-    "\Microsoft\Windows\Application Experience\StartupAppTask"
-    "\Microsoft\Windows\BrokerInfrastructure\BgTaskRegistrationMaintenanceTask"
-    "\Microsoft\Windows\CloudExperienceHost\CreateObjectTask"
-    "\Microsoft\Windows\Defrag\ScheduledDefrag"
-    "\Microsoft\Windows\Device Information\Device"
-    "\Microsoft\Windows\Device Setup\Metadata Refresh"
-    "\Microsoft\Windows\Diagnosis\Scheduled"
-    "\Microsoft\Windows\DiskCleanup\SilentCleanup"
-    "\Microsoft\Windows\DiskFootprint\Diagnostics"
-    "\Microsoft\Windows\InstallService\ScanForUpdates"
-    "\Microsoft\Windows\InstallService\ScanForUpdatesAsUser"
-    "\Microsoft\Windows\InstallService\SmartRetry"
-    "\Microsoft\Windows\Management\Provisioning\Cellular"
-    "\Microsoft\Windows\MemoryDiagnostic\ProcessMemoryDiagnosticEvents"
-    "\Microsoft\Windows\MemoryDiagnostic\RunFullMemoryDiagnostic"
-    "\Microsoft\Windows\MUI\LPRemove"
-    "\Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem"
-    "\Microsoft\Windows\Printing\EduPrintProv"
-    "\Microsoft\Windows\PushToInstall\LoginCheck"
-    "\Microsoft\Windows\Ras\MobilityManager"
-    "\Microsoft\Windows\Registry\RegIdleBackup"
-    "\Microsoft\Windows\RetailDemo\CleanupOfflineContent"
-    "\Microsoft\Windows\Shell\IndexerAutomaticMaintenance"
-    "\Microsoft\Windows\SoftwareProtectionPlatform\SvcRestartTaskNetwork"
-    "\Microsoft\Windows\StateRepository\MaintenanceTasks"
-    "\Microsoft\Windows\Time Synchronization\ForceSynchronizeTime"
-    "\Microsoft\Windows\Time Synchronization\SynchronizeTime"
-    "\Microsoft\Windows\Time Zone\SynchronizeTimeZone"
-    "\Microsoft\Windows\UpdateOrchestrator\Report policies"
-    "\Microsoft\Windows\UpdateOrchestrator\Schedule Scan Static Task"
-    "\Microsoft\Windows\UpdateOrchestrator\Schedule Scan"
-    "\Microsoft\Windows\UpdateOrchestrator\UpdateModelTask"
-    "\Microsoft\Windows\UpdateOrchestrator\USO_UxBroker"
-    "\Microsoft\Windows\UPnP\UPnPHostConfig"
-    "\Microsoft\Windows\WaaSMedic\PerformRemediation"
-    "\Microsoft\Windows\Windows Error Reporting\QueueReporting"
-    "\Microsoft\Windows\Windows Filtering Platform\BfeOnServiceStartTypeChange"
-    "\Microsoft\Windows\WindowsUpdate\Scheduled Start"
-    "\Microsoft\Windows\Wininet\CacheTask"
-    "\Microsoft\XblGameSave\XblGameSaveTask"
-) do (
-	schtasks /change /disable /TN %%a > nul 2>nul
-)
 
 del /f /q %SystemDrive%\unattend.xml
 %windir%\System32\UsoClient.exe RefreshSettings
