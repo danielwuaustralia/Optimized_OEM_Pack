d74a75b5459ab63e3b0cef47ca98855f7220ab01
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2023-03-15 14:32:30 +1100

    Update SetupComplete.cmd

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -13,36 +13,39 @@ setx POWERSHELL_TELEMETRY_OPTOUT 1
 ftype Microsoft.PowerShellScript.1="C:\Program Files\PowerShell\7\pwsh.exe" -NoLogo -ExecutionPolicy Bypass -File "%1" %*
 
 rem DirectX
-start /wait C:\TEMP\DirectX\DXSETUP.exe /silent
+start /wait "C:\TEMP\DirectX\DXSETUP.exe" /silent
 
-rem https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170
-start /wait C:\TEMP\vcredist\2005\vcredist_x86.exe /Q
-start /wait C:\TEMP\vcredist\2005\vcredist_x64.exe /Q
-start /wait C:\TEMP\vcredist\2008\vcredist_x86.exe /q
-start /wait C:\TEMP\vcredist\2008\vcredist_x64.exe /q
-start /wait C:\TEMP\vcredist\2010\vcredist_x86.exe /q /norestart
-start /wait C:\TEMP\vcredist\2010\vcredist_x64.exe /q /norestart
-start /wait C:\TEMP\vcredist\2012\vcredist_x86.exe /install /quiet /norestart
-start /wait C:\TEMP\vcredist\2012\vcredist_x64.exe /install /quiet /norestart
-start /wait C:\TEMP\vcredist\2013\vcredist_x86.exe /install /quiet /norestart
-start /wait C:\TEMP\vcredist\2013\vcredist_x64.exe /install /quiet /norestart
 rem https://forums.mydigitallife.net/threads/abbodi1406s-batch-scripts-repo.74197/page-113#post-1768876
-CertUtil -addstore -f Root "C:\TEMP\vcredist\MicAssDesRoo_2011_03_23.crt"
-CertUtil -addstore -f CA "C:\TEMP\vcredist\MicAssDesPCA2011_2011-06-28.crt"
-start /wait C:\TEMP\vcredist\VC_redist.x86.exe /install /quiet /norestart
-start /wait C:\TEMP\vcredist\VC_redist.x64.exe /install /quiet /norestart
+CertUtil -addstore -f Root "C:\TEMP\vcredist\MicAssDesRoo_2011_03_23.crt
+CertUtil -addstore -f CA "C:\TEMP\vcredist\MicAssDesPCA2011_2011-06-28.crt
+start /wait "C:\TEMP\vcredist\2005\vcredist_x86.exe" /Q
+start /wait "C:\TEMP\vcredist\2005\vcredist_x64.exe" /Q
+start /wait "C:\TEMP\vcredist\2008\vcredist_x86.exe" /q
+start /wait "C:\TEMP\vcredist\2008\vcredist_x64.exe" /q
+start /wait "C:\TEMP\vcredist\2010\vcredist_x86.exe" /q /norestart
+start /wait "C:\TEMP\vcredist\2010\vcredist_x64.exe" /q /norestart
+start /wait "C:\TEMP\vcredist\2012\vcredist_x86.exe" /install /quiet /norestart
+start /wait "C:\TEMP\vcredist\2012\vcredist_x64.exe" /install /quiet /norestart
+start /wait "C:\TEMP\vcredist\2013\vcredist_x86.exe" /install /quiet /norestart
+start /wait "C:\TEMP\vcredist\2013\vcredist_x64.exe" /install /quiet /norestart
+rem https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170
+start /wait "C:\TEMP\vcredist\VC_redist.x86.exe" /install /quiet /norestart
+start /wait "C:\TEMP\vcredist\VC_redist.x64.exe" /install /quiet /norestart
 
 rem https://vulkan.lunarg.com/sdk/home
-start /wait C:\TEMP\VulkanRT-1.3.239.0-Installer.exe /S
+start /wait "C:\TEMP\VulkanRT-1.3.239.0-Installer.exe" /S
 
 rem https://www.amd.com/en/support/chipsets/amd-socket-am4/b550
-start /wait C:\TEMP\AMD.exe /S
+start /wait "C:\TEMP\AMD.exe" /S
 
 rem nvidia driver
-start /wait C:\TEMP\nVidia\setup.exe -s -noreboot -clean
+start /wait "C:\TEMP\nVidia\setup.exe" -s -noreboot -clean
 
 rem https://github.com/M2Team/NanaZip/releases
 Dism /Online /Add-ProvisionedAppxPackage /PackagePath:"C:\TEMP\40174MouriNaruto.NanaZip_2.0.450.0_gnj4mf6z9tkrc.msixbundle" /SkipLicense
+
+rem https://www.chiark.greenend.org.uk/~sgtatham/putty/snapshot.html
+%windir%\System32\msiexec.exe /package "C:\TEMP\putty-64bit-2023-03-08-installer.msi" /quiet /qn /norestart
 
 rem Power Plan
 powercfg -import "C:\TEMP\PowerPlan.pow"
