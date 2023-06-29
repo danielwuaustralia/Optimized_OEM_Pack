c4016616f27aaa172c4720da5c49e027fdf815cc
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2023-06-30 00:16:13 +1000

    Update Setup1.cmd

--- Setup1.cmd
+++ Setup1.cmd
@@ -1,6 +1,6 @@
 @echo on
-color 6
-chcp 65001
+color 0a
+chcp 65001>nul
 setlocal enabledelayedexpansion
 setlocal enableextensions
 
@@ -557,25 +557,7 @@ del /f /q /s "C:\Windows\SysWOW64\OneDriveSettingSyncProvider.dll"
 reg delete "HKCU\Environment" /v "OneDrive" /f
 
 :: MSI Mode
-for %%a in (
-    Win32_NetworkAdapter,
-    Win32_PnPEntity,
-    Win32_SoundDevice,
-    Win32_USBController,
-    Win32_VideoController,
-) do (
-    if "%%a" == "Win32_PnPEntity" (
-        for /f "tokens=*" %%b in ('PowerShell -NoP -C "Get-WmiObject -Class Win32_PnPEntity | Where-Object {$_.PNPClass -eq 'SCSIAdapter'} | Where-Object { $_.PNPDeviceID -like 'PCI\VEN_*' } | Select-Object -ExpandProperty DeviceID"') do (
-            reg add "HKLM\SYSTEM\CurrentControlSet\Enum\%%b\Device Parameters\Interrupt Management\MessageSignaledInterruptProperties" /v "MSISupported" /t REG_DWORD /d "1" /f
-            reg delete "HKLM\SYSTEM\CurrentControlSet\Enum\%%b\Device Parameters\Interrupt Management\Affinity Policy" /v "DevicePriority" /f
-        )
-    ) else (
-        for /f %%b in ('wmic path %%a get PNPDeviceID ^| findstr /l "PCI\VEN_"') do (
-            reg add "HKLM\SYSTEM\CurrentControlSet\Enum\%%b\Device Parameters\Interrupt Management\MessageSignaledInterruptProperties" /v "MSISupported" /t REG_DWORD /d "1" /f
-            reg delete "HKLM\SYSTEM\CurrentControlSet\Enum\%%b\Device Parameters\Interrupt Management\Affinity Policy" /v "DevicePriority" /f
-        )
-    )
-)
+powershell -nop -ep bypass -file "C:\TEMP\interrupt_affinity_auto.ps1"
 
 :: system APP
 set "UWPs=HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Applications"
