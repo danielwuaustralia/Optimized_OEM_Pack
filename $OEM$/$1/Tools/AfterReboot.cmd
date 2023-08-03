041d959e37d3dfb3641c79a3d79bc1ef330e5eb4
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2023-08-03 11:56:34 +1000

    Update AfterReboot.cmd

--- $OEM$/$1/Tools/AfterReboot.cmd
+++ $OEM$/$1/Tools/AfterReboot.cmd
@@ -2,5 +2,8 @@
 color 0a
 start /b /wait C:\Tools\IDM-pawel97.exe
 start /b /wait C:\Tools\ColorControl\ColorControl.exe
+start /b /wait C:\Tools\NVidiaProfileInspector\nvidiaProfileInspector.exe
+powershell -nop -ep bypass -c "Get-ScheduledTask | Where {$_.TaskName -match 'GoogleUpdate' } | Unregister-ScheduledTask -Confirm:$false"
+powershell -nop -ep bypass -c "Get-ScheduledTask | Where {$_.TaskName -match 'StartAllBack' } | Unregister-ScheduledTask -Confirm:$false"
 DISM /Online /Cleanup-Image /StartComponentCleanup /ResetBase
 pause
