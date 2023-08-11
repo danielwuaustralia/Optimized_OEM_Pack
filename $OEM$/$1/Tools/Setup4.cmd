73dbb7b388b5b9744daced2d3d5930065ec55ac6
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2023-08-11 13:35:22 +1000

    Update and rename SetupComplete.cmd to Setup4.cmd

--- $OEM$/$1/Tools/Setup4.cmd
+++ $OEM$/$1/Tools/Setup4.cmd
@@ -3,6 +3,9 @@ color 0a
 start /w C:\TEMP\idman641build18.exe /skipdlgs
 powershell -nop -ep bypass -c "Get-ScheduledTask | Where {$_.TaskName -match 'GoogleUpdateTask' } | Unregister-ScheduledTask -Confirm:$false"
 powershell -nop -ep bypass -c "Get-ScheduledTask | Where {$_.TaskName -match 'StartAllBack' } | Unregister-ScheduledTask -Confirm:$false"
+start /w C:\Tools\Startallback.exe
 start /w C:\Tools\IDM-pawel97.exe
 DISM /online /Cleanup-Image /StartComponentCleanup /ResetBase
+del /f /q /s "C:\Tools\Startallback.exe"
+del /f /q /s "C:\Tools\IDM-pawel97.exe"
 pause
