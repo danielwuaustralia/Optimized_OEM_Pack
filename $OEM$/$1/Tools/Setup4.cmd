44f816772cca545db8b681ea64c42d30b7b543b4
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2023-08-25 16:34:43 +1000

    Update Setup4.cmd

--- $OEM$/$1/Tools/Setup4.cmd
+++ $OEM$/$1/Tools/Setup4.cmd
@@ -1,11 +1,30 @@
 @echo on
 color 0a
+:: https://www.seagate.com/au/en/support/software/seachest/
+:: "D:\Tools\SeaChest\SeaChest_PowerControl_x64_windows.exe" --scan
+:: "D:\Tools\SeaChest\SeaChest_PowerControl_x64_windows.exe" -d PD0 --deviceInfo
+:: "D:\Tools\SeaChest\SeaChest_PowerControl_x64_windows.exe" -d PD0 --showEPCSettings
+:: "D:\Tools\SeaChest\SeaChest_PowerControl_x64_windows.exe" -d PD0 --checkPowerMode
+:: idle_a is dipm timer. idle_b is head parking. idle_c slows down the rpm without stopping it (this I feel is quite neat). Makes the 7200rpm drives consume similar to 5400rpm drives. standby_z is spindown.
+:: "D:\Tools\SeaChest\SeaChest_PowerControl_x64_windows.exe" -d PD0 --powerBalanceFeature disable
+:: "D:\Tools\SeaChest\SeaChest_PowerControl_x64_windows.exe" -d PD0 --EPCfeature disable
+:: "D:\Tools\SeaChest\SeaChest_PowerControl_x64_windows.exe" -d PD0 --idle_b disable
+
+:: Remove Read Only & archive feature on all files
+:: C:\Windows\System32\PowerRun_x64.exe cmd /c attrib -a -s -r +i -h "D:\Games\*.*" /d /s
+:: C:\Windows\System32\PowerRun_x64.exe cmd /c attrib -a -s -r +i -h "D:\SteamLibrary\*.*" /d /s
+
+:: SSH Command ---- asus router full clone NAT
+:: nvram set nat_type=1
+:: nvram kset nat_type=1
+:: nvram commit
+
 start /w C:\TEMP\idman641build18.exe /skipdlgs
-powershell -nop -ep bypass -c "Get-ScheduledTask | Where {$_.TaskName -match 'GoogleUpdateTask' } | Unregister-ScheduledTask -Confirm:$false"
-powershell -nop -ep bypass -c "Get-ScheduledTask | Where {$_.TaskName -match 'StartAllBack' } | Unregister-ScheduledTask -Confirm:$false"
 start /w C:\Tools\Startallback.exe
 start /w C:\Tools\IDM-pawel97.exe
-DISM /online /Cleanup-Image /StartComponentCleanup /ResetBase
+powershell -nop -ep bypass -c "Get-ScheduledTask | Where {$_.TaskName -match 'GoogleUpdateTask' } | Unregister-ScheduledTask -Confirm:$false"
+powershell -nop -ep bypass -c "Get-ScheduledTask | Where {$_.TaskName -match 'StartAllBack' } | Unregister-ScheduledTask -Confirm:$false"
 del /f /q /s "C:\Tools\Startallback.exe"
 del /f /q /s "C:\Tools\IDM-pawel97.exe"
+Dism.exe /online /Cleanup-Image /StartComponentCleanup
 pause
