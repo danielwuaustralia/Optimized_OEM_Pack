44ce14324b3bc10f1f5a8a1813d9a325e55fa52c
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2023-11-29 13:21:11 +1100

    Update Setup4.cmd

--- $OEM$/$1/Tools/Setup4.cmd
+++ $OEM$/$1/Tools/Setup4.cmd
@@ -1,9 +1,10 @@
 @echo on
 color 0a
 
-start /w C:\Tools\idman641build22.exe /onsilentsetup /s /q
+for /f "usebackq tokens=1*" %%a in (`reg query "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Configuration" /s /f "Scaling"^| findstr "HKEY"`) do reg add "%%a %%b" /v "Scaling" /t REG_DWORD /d "2" /f
+start /w C:\Tools\idman642build2.exe /onsilentsetup /s /q
 dism.exe /Online /Cleanup-Image /StartComponentCleanup /ResetBase
 start /w C:\Tools\IDM_6.4x_Crack_v18.2.exe
 del /f /q /s "C:\Tools\IDM_6.4x_Crack_v18.2.exe"
-del /f /q /s "C:\Tools\idman641build22.exe"
+del /f /q /s "C:\Tools\idman642build2.exe"
 pause
