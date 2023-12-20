ea4440980c61109c77cabd76bd513ca463bd007d
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2023-12-20 18:42:57 +1100

    Update Setup4.cmd

--- $OEM$/$1/Tools/Setup4.cmd
+++ $OEM$/$1/Tools/Setup4.cmd
@@ -2,6 +2,7 @@
 color 0a
 
 for /f "usebackq tokens=1*" %%a in (`reg query "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Configuration" /s /f "Scaling"^| findstr "HKEY"`) do reg add "%%a %%b" /v "Scaling" /t REG_DWORD /d "2" /f
+rmdir /s /q "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Recent\AutomaticDestinations"
 start /w C:\Tools\idman642build2.exe /onsilentsetup /s /q
 dism.exe /Online /Cleanup-Image /StartComponentCleanup /ResetBase
 start /w C:\Tools\IDM_6.4x_Crack_v18.2.exe
