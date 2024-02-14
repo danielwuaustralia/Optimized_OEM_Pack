197e18490546498be6c8546dbc1d48af71c1aea2
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2024-02-14 11:21:19 +1100

    Update Setup4.cmd

--- $OEM$/$1/Tools/Setup4.cmd
+++ $OEM$/$1/Tools/Setup4.cmd
@@ -1,11 +1,11 @@
 @echo on
 color 0a
 
-for /f "usebackq tokens=1*" %%a in (`reg query "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\Configuration" /s /f "Scaling"^| findstr "HKEY"`) do reg add "%%a %%b" /v "Scaling" /t REG_DWORD /d "2" /f
+for /f %%i in ('REG QUERY "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /s /f Scaling ^|findstr /ri "Configuration"') do reg add "%%i" /v "Scaling" /t REG_DWORD /d "1" /f
 rmdir /s /q "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Recent\AutomaticDestinations"
-start /w C:\Tools\idman642build2.exe /onsilentsetup /s /q
+C:\Windows\System32\PowerRun_x64.exe /SW:0 cmd /c rmdir /s /q "C:\Program Files (x86)\Microsoft"
+C:\Windows\System32\PowerRun_x64.exe /SW:0 cmd /c rmdir /s /q "C:\Windows\Prefetch"
 dism.exe /Online /Cleanup-Image /StartComponentCleanup /ResetBase
-start /w C:\Tools\IDM_6.4x_Crack_v18.2.exe
-del /f /q /s "C:\Tools\IDM_6.4x_Crack_v18.2.exe"
-del /f /q /s "C:\Tools\idman642build2.exe"
-pause
+del /f /q /s "C:\Tools\IDMcrack.exe"
+del /f /q /s "C:\Tools\idman642build3.exe"
+schtasks /delete /tn "StartAllBack Update" /f
