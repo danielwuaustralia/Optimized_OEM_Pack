a27872ad4e9c6c8ab05e5b3172b68925d23d4381
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2024-10-12 18:27:46 +1100

    Update WIM_Prepare.cmd

--- $OEM$/$1/TEMP/WIM_Prepare.cmd
+++ $OEM$/$1/TEMP/WIM_Prepare.cmd
@@ -7,37 +7,39 @@ color 1f
 :: C:\Tools\PowerRun.exe cmd /c attrib -a -s -h -r "E:\$OEM$\*.*" /s /d
 :: wmic process where name="csrss.exe" CALL setpriority 256
 
-rem Dism /Get-ImageInfo /imagefile:C:\TEMP\install.wim
-rem Dism /Export-Image /SourceImageFile:C:\TEMP\install.wim /SourceIndex:4 /DestinationImageFile:C:\TEMP\install2.wim /CheckIntegrity
-rem dism /Image:C:\TEMP\Work /Add-Package /PackagePath:C:\TEMP\kb5043178.msu
-rem dism /Image:C:\TEMP\Work /Enable-Feature /FeatureName:NetFx3 /All /LimitAccess /Source:C:\TEMP\sxs
+rem Windows Patches
+dism /English /mount-wim /wimfile:C:\TEMP\install.wim /index:1 /mountdir:C:\TEMP\Work
+dism /English /Image:C:\TEMP\Work /Add-Package /PackagePath:C:\TEMP\KB5044384.msu
+dism /English /Image:C:\TEMP\Work /Enable-Feature /FeatureName:NetFx3 /All /LimitAccess /Source:C:\TEMP\sxs
+dism /English /unmount-wim /mountdir:C:\TEMP\Work /commit
+dism /English /Export-Image /SourceImageFile:C:\TEMP\install.wim /SourceIndex:1 /DestinationImageFile:C:\TEMP\install2.wim /CheckIntegrity
+del /f /q C:\TEMP\install.wim
+ren C:\TEMP\install2.wim install.wim
 
-dism /mount-wim /wimfile:C:\TEMP\install.wim /index:1 /mountdir:C:\TEMP\Work
-dism /Image:C:\TEMP\Work /Set-Edition:ServerDatacenter /ProductKey:D764K-2NDRG-47T6Q-P8T8W-YP6DF /AcceptEula
-rem Dism /Image:C:\TEMP\Work /Get-Features
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:SmbDirect
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:Printing-PrintToPDFServices-Features
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:Xps-Foundation-Xps-Viewer
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:Windows-Defender
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:Server-Drivers-Printers
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:WindowsServerBackupSnapin
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:RSAT
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:FileAndStorage-Services
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:Storage-Services
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:WorkFolders-Client
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:SystemDataArchiver
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:ServerCoreFonts-NonCritical-Fonts-BitmapFonts
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:ServerCoreFonts-NonCritical-Fonts-MinConsoleFonts
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:ServerCoreFonts-NonCritical-Fonts-Support
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:ServerCoreFonts-NonCritical-Fonts-TrueType
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:ServerCoreFonts-NonCritical-Fonts-UAPFonts
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:SearchEngine-Client-Package
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /Disable-Feature /featurename:WindowsAdminCenterSetup
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /enable-feature /featurename:ServerMediaFoundation /all
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /enable-feature /featurename:LegacyComponents /all
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /enable-feature /featurename:DirectPlay /all
-dism /scratchdir:C:\TEMP\Scratch /image:C:\TEMP\Work /enable-feature /featurename:NetFx4 /all
-rem DISM /Image:C:\TEMP\Work /Get-Capabilities
+rem packages and options
+dism /English /mount-wim /wimfile:C:\TEMP\install.wim /index:1 /mountdir:C:\TEMP\Work
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:SmbDirect
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:Printing-PrintToPDFServices-Features
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:Xps-Foundation-Xps-Viewer
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:Windows-Defender
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:Server-Drivers-Printers
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:WindowsServerBackupSnapin
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:RSAT
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:FileAndStorage-Services
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:Storage-Services
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:WorkFolders-Client
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:SystemDataArchiver
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:ServerCoreFonts-NonCritical-Fonts-BitmapFonts
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:ServerCoreFonts-NonCritical-Fonts-MinConsoleFonts
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:ServerCoreFonts-NonCritical-Fonts-Support
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:ServerCoreFonts-NonCritical-Fonts-TrueType
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:ServerCoreFonts-NonCritical-Fonts-UAPFonts
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:SearchEngine-Client-Package
+dism /English /image:C:\TEMP\Work /Disable-Feature /featurename:WindowsAdminCenterSetup
+dism /English /image:C:\TEMP\Work /enable-feature /featurename:ServerMediaFoundation /all
+dism /English /image:C:\TEMP\Work /enable-feature /featurename:LegacyComponents /all
+dism /English /image:C:\TEMP\Work /enable-feature /featurename:DirectPlay /all
+dism /English /image:C:\TEMP\Work /enable-feature /featurename:NetFx4 /all
 Dism /Image:C:\TEMP\Work /Add-Capability /CapabilityName:WMIC~~~~
 Dism /Image:C:\TEMP\Work /Remove-Capability /CapabilityName:App.StepsRecorder~~~~0.0.1.0
 Dism /Image:C:\TEMP\Work /Remove-Capability /CapabilityName:AzureArcSetup~~~~
@@ -50,9 +52,9 @@ Dism /Image:C:\TEMP\Work /Remove-Capability /CapabilityName:OpenSSH.Client~~~~0.
 Dism /Image:C:\TEMP\Work /Remove-Capability /CapabilityName:OpenSSH.Server~~~~0.0.1.0
 Dism /Image:C:\TEMP\Work /Remove-Capability /CapabilityName:Microsoft.Windows.Sense.Client~~~~
 Dism /Image:C:\TEMP\Work /Remove-Capability /CapabilityName:XPS.Viewer~~~~0.0.1.0
-
-dism /unmount-wim /mountdir:C:\TEMP\Work /commit
-dism /Export-Image /SourceImageFile:C:\TEMP\install.wim /SourceIndex:1 /DestinationImageFile:C:\TEMP\install2.wim /CheckIntegrity
+dism /English /unmount-wim /mountdir:C:\TEMP\Work /commit
+dism /English /Export-Image /SourceImageFile:C:\TEMP\install.wim /SourceIndex:1 /DestinationImageFile:C:\TEMP\install2.wim /Compress:max
 del /f /q C:\TEMP\install.wim
 ren C:\TEMP\install2.wim install.wim
+dism /English /Export-Image /SourceImageFile:C:\TEMP\install.wim /SourceIndex:1 /DestinationImageFile:C:\TEMP\install.esd /Compress:recovery
 pause
