bb04d62b346e3268fc0d68ff0125b09d3b829fd5
Daniel Wu <16550029+danielwuaustralia@users.noreply.github.com>
2024-12-11 11:59:44 +1100

    Update StartUp.cmd

--- $OEM$/$1/Tools/StartUp.cmd
+++ $OEM$/$1/Tools/StartUp.cmd
@@ -1,25 +1,17 @@
 @echo off
 
-:: Remove Read Only & archive feature on all files
-:: C:\Tools\PowerRun.exe cmd /c attrib -a -s -h -r "D:\Games\*.*" /s /d
-:: C:\Tools\PowerRun.exe cmd /c attrib -a -s -h -r "D:\SteamLibrary\*.*" /s /d
-:: C:\Tools\PowerRun.exe cmd /c attrib -a -s -h -r "D:\$OEM$\*.*" /s /d
-:: wmic process where name="csrss.exe" CALL setpriority 256
-
-:: C:\Tools\PowerRun.exe cmd /c logman stop SleepStudyTraceSession -ets
+C:\Tools\DeviceCleanupCmd.exe * -s -n
+net start stornvmeofi
+net stop AppXSvc
 C:\Tools\PowerRun.exe /SW:0 cmd /c rmdir /s /q "C:\TEMP"
-C:\Tools\PowerRun.exe /SW:0 cmd /c rmdir /s /q "C:\Users\Administrator\AppData\Local\NVIDIA\DXCache"
-C:\Tools\PowerRun.exe /SW:0 cmd /c rmdir /s /q "C:\Users\Administrator\AppData\Local\NVIDIA\GLCache"
+C:\Tools\PowerRun.exe /SW:0 cmd /c rmdir /s /q "C:\Users\Administrator\AppData\Local\NVIDIA"
+C:\Tools\PowerRun.exe /SW:0 cmd /c rmdir /s /q "C:\Users\Administrator\AppData\LocalLow\NVIDIA\PerDriverVersion\DXCache"
 C:\Tools\PowerRun.exe /SW:0 cmd /c taskkill /f /im ctfmon.exe
 C:\Tools\PowerRun.exe /SW:0 cmd /c del /f /q "C:\Windows\System32\ctfmon.exe"
 C:\Tools\PowerRun.exe /SW:0 cmd /c taskkill /f /im ChsIME.exe
 C:\Tools\PowerRun.exe /SW:0 cmd /c del /f /q "C:\Windows\System32\InputMethod\CHS\ChsIME.exe"
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\WmiApRpl\Performance" /v "Disable Performance Counters" /t REG_DWORD /d "1" /f
 powershell -noprofile -executionpolicy bypass -command "Set-DisplayRefreshRate -DisplayId 1 -RefreshRate 144"
 powershell -executionpolicy bypass -file "C:\Tools\fix_mouse_keyboard_priority_bug.ps1"
-ipconfig /flushdns
-
-:: CleanMgr.exe
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Active Setup Temp Folders" /v "StateFlags0001" /f
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\BranchCache" /v "StateFlags0001" /f
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Content Indexer Cleaner" /v "StateFlags0001" /f
@@ -51,9 +43,7 @@ reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Windows ESD installation files" /v "StateFlags0001" /f
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Windows Reset Log Files" /v "StateFlags0001" /f
 reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Windows Upgrade Log Files" /v "StateFlags0001" /f
-
-timeout /t 2 /nobreak
-
+timeout /t 1 /nobreak
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Active Setup Temp Folders" /v "StateFlags0001" /t REG_DWORD /d "2" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\BranchCache" /v "StateFlags0001" /t REG_DWORD /d "2" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Content Indexer Cleaner" /v "StateFlags0001" /t REG_DWORD /d "2" /f
@@ -86,5 +76,4 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Wi
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Windows Reset Log Files" /v "StateFlags0001" /t REG_DWORD /d "2" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Windows Upgrade Log Files" /v "StateFlags0001" /t REG_DWORD /d "2" /f
 start /high CLEANMGR /sagerun:1
-
 exit
